2eca8ec3dddaf1859db7785c47a3d62e1fd94c2c
Palma Vázquez, Julio <julio.palma.vazquez@accenture.com>
2017-10-01 05:16:55 +0200

    New multiple annotations demo

--- deors.demos.annotations.base.processors/src/main/java/deors/demos/annotations/base/processors/ComplexityProcessor.java
+++ deors.demos.annotations.base.processors/src/main/java/deors/demos/annotations/base/processors/ComplexityProcessor.java
@@ -1,5 +1,7 @@
 package deors.demos.annotations.base.processors;
 
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -11,6 +13,7 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 
+import deors.demos.annotations.base.Author;
 import deors.demos.annotations.base.Complexity;
 
 /**
@@ -19,7 +22,7 @@ import deors.demos.annotations.base.Complexity;
  * @author deors
  * @version 1.0
  */
-@SupportedAnnotationTypes("deors.demos.annotations.base.Complexity")
+@SupportedAnnotationTypes({"deors.demos.annotations.base.Complexity", "deors.demos.annotations.base.Author"})
 @SupportedSourceVersion(SourceVersion.RELEASE_9)
 public class ComplexityProcessor
     extends AbstractProcessor {
@@ -43,16 +46,36 @@ public class ComplexityProcessor
      */
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-
-        for (Element elem : roundEnv.getElementsAnnotatedWith(Complexity.class)) {
-
-            Complexity complexity = elem.getAnnotation(Complexity.class);
-
-            String message = "annotation found in " + elem.getSimpleName()
-                + " with complexity " + complexity.value();
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, message);
+	
+        for (Element elem : roundEnv.getElementsAnnotatedWithAny(getAnnotationTypes())) {
+        
+        	for (Complexity c : elem.getAnnotationsByType(Complexity.class)) {
+	
+	            String message = "Annotation found in " + elem.getSimpleName()
+	                + " with complexity " + c.value();
+	            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, message);
+        	}
+        	
+        	for (Author a : elem.getAnnotationsByType(Author.class)) {
+        		
+        		String message = "Annotation found in " + elem.getSimpleName()
+        			+ " from author " + a.name();
+        		processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, message);
+        	}
         }
 
         return true;
+    }
+    
+    
+    /**
+     * Returns the annotation types to consider
+     * @return annotation types to consider
+     */
+    private Set<Class<? extends Annotation>> getAnnotationTypes() {
+    	Set<Class<? extends Annotation>> annotationTypes = new HashSet<>();
+    	annotationTypes.add(Complexity.class);
+    	annotationTypes.add(Author.class);
+    	return annotationTypes;
     }
 }
